b509a41d68cc62bd819ffc0ca254837f1c70a646
jianlin.zjl <jianlin.zjl@alibaba-inc.com>
2017-01-03 17:35:21 +0800

    push

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const types = require('babel-types');
 const util = require('util');
 module.exports = function (t,options) {
-    t = t.types || {};
+    var types = t.types || {};
     const platformMap = {
         weex: 'isWeex',
         web: 'isWeb',
@@ -37,49 +36,8 @@ module.exports = function (t,options) {
                     //替换
                     path.replaceWith(objectExpressionMethod(options.platform));
                 }
-            },
-            /*ImportDeclaration(path,options) {
-                const { node } = path;
-                options = util._extend({
-                    name: 'universal-env',
-                },options.opts || {});
-                console.log(22)
-               /!* if (node.source.value === options.name) {
-                    node.specifiers.forEach(spec => {
-                        specified.push({
-                        local: spec.local.name,
-                        imported: spec.imported.name
-                    });
-                });
+            }
 
-                    if (options.platform) {
-                        specified.forEach(specObj => {
-                            let newNodeInit = specObj.imported === platformMap[options.platform] ?
-                                true : false;
-                        let newNode = variableDeclarationMethod(
-                            specObj.imported,
-                            newNodeInit
-                        );
-
-                        path.insertAfter(newNode);
-
-                        // Support custom alise import:
-                        // import { isWeex as iw } from 'universal-env';
-                        // const isWeex = true;
-                        // const iw = true;
-                        if (specObj.imported !== specObj.local) {
-                            newNode = variableDeclarationMethod(
-                                specObj.local,
-                                newNodeInit
-                            );
-                            path.insertAfter(newNode);
-                        }
-                    });
-
-                        path.remove();
-                    }
-                }*!/
-            }*/
         }
     };
 }
